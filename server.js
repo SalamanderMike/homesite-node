b77ed7adbd11abdc1dc5b0bdf6e37bf13d3783d4
Michael McClure <salamandermike@gmail.com>
2017-03-24 16:06:54 -0700

    Bugfix Dependencies

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var opbeat = require('opbeat').start({
-		appId: process.env.OPBEAT_APP_ID,
-		organizationId: process.env.OPBEAT_ORGANIZATION_ID,
-		secretToken: process.env.OPBEAT_SECRET_TOKEN
-	}),															// OPBEAT ANALYTICS
+var 
+	// opbeat = require('opbeat').start({
+	// 	appId: process.env.OPBEAT_APP_ID,
+	// 	organizationId: process.env.OPBEAT_ORGANIZATION_ID,
+	// 	secretToken: process.env.OPBEAT_SECRET_TOKEN
+	// }),															// OPBEAT ANALYTICS
 	express = require('express'),
 	bodyParser = require('body-parser'),
 	path = require('path'),
@@ -24,7 +25,7 @@ app.use('/locales', express.static(path.join(__dirname + '/locales')));						// 
 app.use('/bower_components', express.static(path.join(__dirname + '/bower_components')));	// PLACE BOWER REQUIREMENTS
 app.use(express.static(__dirname + '/assets'));
 app.set('view engine', 'ejs');
-app.use(opbeat.middleware.express());							// ANALYTICS
+// app.use(opbeat.middleware.express());							// ANALYTICS
 
 
 // APIs
